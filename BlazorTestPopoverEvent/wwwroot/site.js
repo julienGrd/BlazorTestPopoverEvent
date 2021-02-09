6a8a77163e5a7f5dae3b7916b7adf1d69f83d716
julien gourdon <jul.gourdon@gmail.com>
2021-02-09 11:13:36 +0100

    Ajoutez des fichiers projet.

---
+++ BlazorTestPopoverEvent/wwwroot/site.js
@@ -0,0 +1,28 @@
+﻿//this function show correctly the popover but the event inside not work
+popover: function popover(selector, contentSelector) {
+    $(selector).popover({
+        html: true,   
+        sanitize: false,
+        content: function () {
+            return $(contentSelector).html();
+        },
+        //container: 'app'
+    })
+}
+
+//this function is supposed to be the correction but im unable to make it work
+//popover: function popover(selector, contentSelector) {
+//    var contentParent;
+
+//    $(selector).popover({
+//        html: true,
+//        sanitize: false,
+//        content: function () {
+//            return $(contentSelector);
+//        },
+//    }).on('show.bs.popover', function () {
+//        contentParent = $(contentSelector).parent();
+//    }).on('hide.bs.popover', function () {
+//        contentParent.append($(contentSelector));
+//    });
+//}
